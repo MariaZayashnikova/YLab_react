bac4e73548e078dc259cbf073d8deabacee1c693
Mariya_Zaya <mariyazayaschnikova@gmail.com>
2023-06-02 16:12:13 +0300

    fix

--- src/app/profile/index.js
+++ src/app/profile/index.js
@@ -1,9 +1,7 @@
-import {memo, useCallback, useEffect} from 'react';
+import {memo, useEffect} from 'react';
 import {useNavigate} from "react-router-dom";
 import useSelector from "../../hooks/use-selector";
 import useTranslate from "../../hooks/use-translate";
-import useStore from "../../hooks/use-store";
-import useInit from "../../hooks/use-init";
 import PageLayout from "../../components/page-layout";
 import Head from "../../components/head";
 import Navigation from "../../containers/navigation";
